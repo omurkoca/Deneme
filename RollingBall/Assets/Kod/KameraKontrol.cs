d3e2e63d0d2c367aeb1aa4748d82d757ff2d779b
omurkoca <omurkoca.omr@gmail.com>
2018-01-14 15:47:29 +0300

    Rolling Ball

---
+++ RollingBall/Assets/Kod/KameraKontrol.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraKontrol : MonoBehaviour {
+
+    public GameObject top;
+    Vector3 aradakiMesafe;
+	void Start ()
+    {
+        //transform.position kameranın içinde kod yazdığımız için kameranın pozisyonuna ulaşabiliyoruz, Fakat topun pozisyonuna ulaşamıyoruz
+        //top.transform.position GameObject oluşturduk ve unityde topu ona atadık. Böylece erişebiliyoruz.
+        aradakiMesafe = transform.position - top.transform.position;
+        //Oyunun başında bir kere mesafeyi bulduruyoruz ve her yerinde kullanıyoruz.
+	}
+	
+	
+	void LateUpdate () //Bütün updateler bitince çalışıyor
+    {
+        // transform.position = top.transform.position; kamera topun içine girer
+        transform.position = top.transform.position + aradakiMesafe;
+	}
+}
---
+++ RollingBall/Assets/Kod/TopKontrol.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour {
+    Rigidbody fizik;
+    public int hiz;
+    int sayac = 0;
+    public int ToplanacakObjeSayisi;
+    public Text txtSayac;
+    public Text txtOyunBitti;
+    void Start ()
+    {
+        fizik = GetComponent<Rigidbody>();//Componente erişme	
+	}
+	
+	void FixedUpdate ()
+    {
+        //Sürekli çalışır fakat sabit bir hızda sürekli.
+
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+        //Debug.Log("yatay = " +yatay+ "    dikey = "+dikey);
+
+
+        Vector3 vec = new Vector3(yatay, 0, dikey);//3 boyutlu objelerin x, y, z koordinatlarında hareketini sağlıyor.
+        fizik.AddForce(vec * hiz);//AddForce vector3 deki değerleri atayabiliyoruz.
+
+	}
+
+    void OnTriggerEnter(Collider other)//Enter Stay ve Exit metodları var
+    {
+        //Enter temas halinde çalışıyor
+        //Stay Temas olduğu süre boyunca bize değer dönüyor.
+        //--Kullanıcı yakalandığı zaman bununla ayarlayacaz
+        //Exit Temastan çıkınca yazdırıyor
+        //Destroy(other.gameObject);//Temas edilen objeyi yok et diyoruz
+        //Böyle olursaa triggeri açık olan her gameObject yok olur.
+        if (other.tag == "Engel")
+        {
+            Destroy(other.gameObject);
+            //other.gameObject.SetActive(false); //Görünürlüğünü kaldırıyorsun, Enable False ve Visible False gibi düşün
+            sayac++;
+
+            txtSayac.text = "Sayac = " + sayac;
+
+            if (sayac == ToplanacakObjeSayisi)
+            {
+                txtOyunBitti.text = "Oyun Bitti.";
+            }
+        }
+    }
+
+}
